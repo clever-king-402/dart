b84e560f14050bf41a009e8cdcf34e6b9cc7e650
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-11-27 21:14:23 +0545

    Add files via upload

---
+++ comment.dart
@@ -0,0 +1,12 @@
+// this is single line comment
+
+/*
+  this is multi-line comment
+/*
+
+// book clean code uncle bob
+
+// paramemter limit
+/* good function name
+// function should have 2 work max
+dont write comment everywhere */
---
+++ variable.dart
@@ -0,0 +1,46 @@
+main() {
+  String first_name = "ram";
+  String firstName = "ram";
+  int score = 40;
+  double perce = 40.43;
+  bool isPass = true;
+
+  List students = ["ram", "shyam"];
+  students.add("sita");
+  List studentCollection = ["hello", "this", "good"];
+  students.insert(0, "sita");
+  students.insertAll(0, studentCollection);
+  // students.addAll(studentCollection);
+
+  print(students.remove("ram"));
+  students.removeAt(3);
+  students.removeLast();
+  students.removeRange(0, 2);
+  print(students);
+
+  print(isPass);
+  print(isPass);
+
+//for string
+  /*
+      ''
+      ""
+      ''''''
+  */
+
+  String type1 = 'type1';
+  String type2 = "type2";
+  String type3 = '''type3
+  sdflaksfaf
+  asdfsf''';
+
+  String name = "ranjit";
+  String address = "dryport";
+  print("my name is " + name + ". My address is " + address);
+  print("my name is \"$name\". My Address is $address. my age is ${5} ");
+  /*
+  Camel Case => myFirstNameIsRam lower MyFirstNameIsRam upper => used for variable name and object name
+  Pascel Case => MyNameIsRam => used for class names
+  snake case => frist_name_is_good  => used for file name and folders
+  */
+}
